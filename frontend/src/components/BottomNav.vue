e86444534e5e7af7f3d0da7683854a56028a6022
KarineMailletENI <karine.maillet2025@campus-eni.fr>
2026-06-22 11:19:38 +0200

    Création de la barre de navigation. Création de la vue home. Import de lucide dans le projet.

---
+++ frontend/src/components/BottomNav.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import { Compass, Luggage, ListTodo, UserRound } from '@lucide/vue'
+  import {ref} from 'vue'
+
+  const activeTab = ref('explorer')
+</script>
+
+<template>
+    <nav>
+      <div :class="{active: activeTab === 'explorer'}" @click="activeTab = 'explorer'">
+        <Compass />
+        <p>Explorer</p>
+      </div>
+      <div :class="{active: activeTab === 'voyages'}" @click="activeTab = 'voyages'">
+        <Luggage />
+        <p>Mes voyages</p>
+      </div>
+      <div :class="{active: activeTab === 'listes'}" @click="activeTab = 'listes'">
+        <ListTodo />
+        <p>Listes</p>
+      </div>
+      <div :class="{active: activeTab === 'profil'}" @click="activeTab = 'profil'">
+        <UserRound />
+        <p>Profil</p>
+      </div>
+    </nav>
+</template>
+
+<style scoped>
+  nav {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+    justify-content: space-evenly;
+    border-top: 1px solid var(--color-soie-orientale);
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .active {
+    color: var(--color-mer-mediterranee);
+  }
+</style>

52e7aac524338bfb26d02b57dc1032ac4bbc05d4
KarineMailletENI <karine.maillet2025@campus-eni.fr>
2026-08-28 14:24:00 +0200

    Front - Changement de mascotte Modification du front de HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,47 @@
 <script setup>
   import BottomNav from "@/components/BottomNav.vue";
+  import { MoveRight, CirclePlus, ChevronRight } from '@lucide/vue';
 </script>
 
 <template>
   <main>
-    <!-- header, cards, button CTA -->
     <header>
-      <div>
         <h2>Salut Karine !</h2>
         <h3>Où s'envole-t-on aujourd'hui ?</h3>
-      </div>
-      <img src="@/assets/mascotte.png" alt="La mascotte nommée Hira parle">
     </header>
+    <div class="cards">
+      <div class="departure-block">
+        <img src="@/assets/mascotte-happy.png" alt="La mascotte nommée Hira parle">
+        <div class="card-accent card">
+          <div class="card-accent-contain">
+            <h4 class="card-title">Prochain départ</h4>
+            <strong>J-5</strong>
+            <h5>Cap sur Lisbonne </h5>
+          </div>
+          <span class="button-accent">
+            Voir plus <MoveRight />
+          </span>
+        </div>
+      </div>
+      <div class="card-sample card">
+        <h4 class="card-title">Voyage à compléter</h4>
+        <div class="card-contain">
+          <svg width="36" height="36" viewBox="0 0 52 52" aria-hidden="true">
+            <circle cx="26" cy="26" r="21" fill="none" stroke="#FFFDFC" stroke-width="6"></circle>
+            <circle cx="26" cy="26" r="21" fill="none" stroke="#05272B" stroke-width="6" stroke-linecap="round" stroke-dasharray="105.6 131.9" transform="rotate(-90 26 26)"></circle>
+          </svg>
+          <div>
+            <h5>Voyage au Japon</h5>
+            <p>12 éléments bouclés sur 15</p>
+          </div>
+        </div>
+        <ChevronRight />
+      </div>
+      <div class="card-CTA card">
+        <CirclePlus />
+        <h4>Organise ton nouveau voyage</h4>
+      </div>
+    </div>
   </main>
   <BottomNav />
 </template>
@@ -25,26 +55,70 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-bottom: 60px;
   }
-  header > div {
-    text-align: center;
-    background-color: var(--color-soie-orientale);
-    border-radius: 50% 50% 57% 43% / 66% 69% 31% 34% ;
-    padding: 20px;
+  .departure-block {
+    display: flex;
+    flex-direction: row;
     position: relative;
   }
-  header > div::after {
-    content: '';
-    position: absolute;
-    bottom: -10px;
-    left: 40%;
-    border-left: 10px solid transparent;
-    border-right: 20px solid transparent;
-    border-top: 20px solid var(--color-soie-orientale);
-  }
-  header > img {
-    width: 125px;
+  .departure-block > img {
+    width: 180px;
     transform: scaleX(-1);
-    margin-left: -150px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 2;
+  }
+  .cards > .departure-block > .card-accent{
+    margin-left: 150px;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .cards > .departure-block > .card-accent > .card-accent-contain > strong{
+    font-size: 35px;
+  }
+  .cards > .departure-block > .card-accent > .card-accent-contain {
+    margin-top: 10px;
+  }
+  .lucide{
+    vertical-align: middle;
+  }
+  .lucide-move-right{
+    font-size: smaller;
+  }
+  .card-sample{
+    position: relative;
+  }
+  .card-sample > .card-title{
+    padding-bottom: 10px;
+  }
+  .card-sample > .card-contain{
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    justify-content: left;
+  }
+  .card-sample > .card-contain > div {
+    padding-inline-start: 10px;
+  }
+  .card-sample > .card-contain > div >  p{
+    font-size: 12px;
+  }
+  .card-sample > svg {
+    position : absolute;
+    top: 50%;
+    transform : translateY(-50%);
+    right: 5px;
+  }
+  .cards > .card-CTA {
+    display: inline-flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  .cards > .card-CTA > svg{
+    margin-inline: 7px;
   }
 </style>
